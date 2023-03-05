35359772f5489f34de3b69d15b174cbb664f45e6
nadirkaraman2001 <nadirkaraman2001@gmail.com>
2023-03-05 23:07:58 +0300

    Created a preprocessor class and different types of embedders

--- main.py
+++ main.py
@@ -2,34 +2,70 @@ import sister
 import numpy as np
 import gensim.downloader as api
 
-from preprocessor import __filter
+from preprocessor import Preprocessor
+
+class Embedder:
+    def __init__(self, preprocessor) -> None:
+        print("in embedder")
+        self.model = None
+        self.preprocessor = preprocessor
+
+    def get_embedding(self, sentence) -> np.array(float):
+        """override this method"""
+        pass
 
 
-def wordtovec_embedding(model, sentence):
-    filtered_sentence = __filter(sentence)
+class BertEmbedder(Embedder):
+    def __init__(self, preprocessor):
+        print("in bert embedder")
+        super().__init__(preprocessor)
+        self.model = sister.BertEmbedding(lang='en')
 
-    # word2vec embedding of each word, combined in a 2d list
-    result = np.array([model[w] for w in filtered_sentence.split() if w in model])
+    def get_embedding(self, sentence):
+        filtered_sentence = self.preprocessor.filter(sentence)
+        return self.model.embed(sentences=[sentence])
+    
+    def get_multiple_embeddings(self, sentences: list[str]):
+        return self.model.embed(sentences=sentences)
 
-    # the mean value for each index in vectors
-    mean = np.mean(result, axis=0)
 
-    return mean.reshape(1, -1)
+class Word2VecEmbedder(Embedder):
+    def __init__(self, preprocessor):
+        print("in w2v embedder")
+
+        super().__init__(preprocessor)
+        self.model = api.load('word2vec-google-news-300')
+    
+    
+    def get_embedding(self, sentence):
+        filtered_sentence = self.preprocessor.filter(sentence)
+
+        # word2vec embedding of each word, combined in a 2d list
+        result = np.array([self.model[w] for w in filtered_sentence.split() if w in self.model])
+
+        # the mean value for each index in vectors
+        mean = np.mean(result, axis=0)
+
+        return mean.reshape(1, -1)
+    
 
 
 def main(): 
+    preprocessor = Preprocessor()
+
     # word2vec model pretrained with Google News dataset
-    wv_model = api.load('word2vec-google-news-300')
+    w2v_embedder = Word2VecEmbedder(preprocessor=preprocessor)
 
     # pretrained bert model
-    bert_embedder = sister.BertEmbedding(lang='en')
-
+    bert_embedder = BertEmbedder(preprocessor=preprocessor)
+    
     sentence = "London is the capital of Great Britain"
-    w2v_result = wordtovec_embedding(model=wv_model, sentence=sentence)
-    bert_result = bert_embedder.embed(sentences=[sentence])
+    w2v_result = w2v_embedder.get_embedding(sentence=sentence)
+    bert_result = bert_embedder.get_embedding(sentence=sentence)
 
-    print("word2vec result \n", w2v_result.shape, w2v_result)
-    print("bert result \n", bert_result.shape, bert_result)
+    print("word2vec result \n", w2v_result.shape)
+    print("bert result \n", bert_result.shape)
+    
 
 
 if __name__ == '__main__':
--- preprocessor.py
+++ preprocessor.py
@@ -10,49 +10,58 @@ from nltk.corpus import stopwords
 # Preprocesses the dataset and saves it in .json format to location.
 # Running this script once is enough to generate preprocessed dataset.
 def main():
-    dataset = ir_datasets.load("cranfield")
-    nltk.download('stopwords')
-    nltk.download('punkt')
-    location = "./resources/cranfield_preprocessed.json"
+    preprocessor = Preprocessor()
+    s = "hi my name is enis, i am 22 years old, my blog site: https://enis.com"
+    print(preprocessor.filter(s)) # --> hi name enis years old blog site
+
+
+class Preprocessor:
+    def __init__(self):
+        self.dataset = ir_datasets.load("cranfield")
+        self.location = "./resources/cranfield_preprocessed.json"
+
+        nltk.download('stopwords')
+        nltk.download('punkt')
     
-    preprocessed = preprocess(dataset)
-    json_str = json.dumps(preprocessed, ensure_ascii=False, indent=3)
+        preprocessed = self.preprocess(self.dataset)
+        json_str = json.dumps(preprocessed, ensure_ascii=False, indent=3)
 
-    with open(location, "w") as docs_file:
-        docs_file.write(json_str)
+        with open(self.location, "w") as docs_file:
+            docs_file.write(json_str)
 
 
-# Preprocesses title and text field in each document in the dataset and return
-# the result as a dictionary.
-def preprocess(dataset) -> dict:
-    res = {"docs": []}
+    # Preprocesses title and text field in each document in the dataset and return
+    # the result as a dictionary.
+    def preprocess(self, dataset) -> dict:
+        res = {"docs": []}
 
-    for doc in dataset.docs_iter():
-        new_doc = {}
+        for doc in dataset.docs_iter():
+            new_doc = {}
+            
+            new_doc["doc_id"] = doc.doc_id
+            new_doc["title"] = self.filter(doc.title)
+            new_doc["text"] = self.filter(doc.text)
+            new_doc["author"] = doc.author
+            new_doc["bib"] = doc.bib
+
+            res["docs"].append(new_doc)
+
+        return res
+    
+
+    # ! Requires these two to be executed previously: 
+    #   - nltk.download('stopwords')
+    #   - nltk.download('punkt')
+    def filter(self, text: str) -> str:
+        # remove links, numbers and special characters
+        text = re.sub(r"http\S+", "", text)
+        text = re.sub("[^A-Za-z]+", " ", text)
         
-        new_doc["doc_id"] = doc.doc_id
-        new_doc["title"] = __filter(doc.title)
-        new_doc["text"] = __filter(doc.text)
-        new_doc["author"] = doc.author
-        new_doc["bib"] = doc.bib
+        # remove stopwords
+        tokens = nltk.word_tokenize(text)
+        tokens = [w.lower().strip() for w in tokens if not w.lower() in stopwords.words("english")]
 
-        res["docs"].append(new_doc)
-
-    return res
-
-# ! Requires these two to be executed previously: 
-#   - nltk.download('stopwords')
-#   - nltk.download('punkt')
-def __filter(text: str) -> str:
-    # remove links, numbers and special characters
-    text = re.sub(r"http\S+", "", text)
-    text = re.sub("[^A-Za-z]+", " ", text)
-    
-    # remove stopwords
-    tokens = nltk.word_tokenize(text)
-    tokens = [w.lower().strip() for w in tokens if not w.lower() in stopwords.words("english")]
-
-    return "".join(token + " " for token in tokens)
+        return "".join(token + " " for token in tokens)
 
 
 if __name__ == "__main__":
